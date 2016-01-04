baebbf70e619334c3495326c8ef7e1fddb993149
roma.safin@me.com <roma.safin@me.com>
2016-01-04 18:29:48 +0200

    Initial Commit

---
+++ CheTamUHohlov/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CheTamUHohlov
+//
+//  Created by Roman.Safin on 1/4/16.
+//  Copyright © 2016 Roman.Safin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
