db58b3e80cb077edc86de0d425c95462015c48cb
roma.safin@me.com <roma.safin@me.com>
2016-01-31 15:22:34 +0200

    added jokes class

---
+++ CheTamUHohlov/FunnyJokesClass.h
@@ -0,0 +1,18 @@
+//
+//  FunnyJokesClass.h
+//  CheTamUHohlov
+//
+//  Created by Roman.Safin on 1/27/16.
+//  Copyright © 2016 Roman.Safin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+typedef enum countryType {
+    Ukraine,
+    Russia
+} CountryTypeEnum ;
+
+
+@interface FunnyJokesClass : NSObject
+
+@end
